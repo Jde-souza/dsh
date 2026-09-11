9832760a37b770e3fb8144eb8f03484579d813f7
Jonatan <jdsmuy@gmail.com>
2026-09-10 22:21:29 -0300

    fix: blog/:slug usa RenderMode.Server para evitar crash en Netlify Edge

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,7 +3,7 @@ import {RenderMode, ServerRoute} from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'blog/:slug',
-    renderMode: RenderMode.Client,
+    renderMode: RenderMode.Server,
   },
   {
     path: '**',
